58b3c970fbfe63c596570c2933edb98840c8f05d
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-16 16:24:16 -0300

    Commit Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento App', 'Front-end web']); 
+    function handleAddProject() {
+        setProjects([...projects, `Novo projeto ${Date.now()}` ]);
+    }
     return (
       <>  
-       <Header title="Homepage">
-           <ul>
-               <li>
-                   Homepage
-               </li>
-               <li>
-                   Details
-               </li>
-           </ul>
-       </Header>
-       <Header title="Projects">
-       <ul>
-               <li>
-                   SubHomepage
-               </li>
-               <li>
-                   Topics
-               </li>
-               <li>
-                   SubTopics
-               </li>
-           </ul>
-       </Header>
+       <Header title="Projects" />
+          <ul>
+            {projects.map(project => <li key={project}>{project}</li>)}
+          </ul>
+          <button type="button" onClick={handleAddProject}>
+              Adicionar Projeto
+          </button>
       </>
     )
 }
